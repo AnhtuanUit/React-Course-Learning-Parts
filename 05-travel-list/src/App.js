8a4e144d34e20e0f03beed21c27a24092f449ca9
TuanDo <anhtuan7194@gmail.com>
2024-01-27 18:39:44 +0700

    Sorting Items

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -82,10 +82,25 @@ function Form({ onAddItems }) {
 }
 
 function PackageList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortItems;
+  if (sortBy === 'input') {
+    sortItems = items;
+  }
+
+  if (sortBy === 'description') {
+    sortItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === 'packed') {
+    sortItems = items.slice().sort((a, b) => a.packed - b.packed);
+  }
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortItems.map(item => (
           <Item
             key={item.id}
             item={item}
@@ -94,6 +109,13 @@ function PackageList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="action">
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by desciption</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
