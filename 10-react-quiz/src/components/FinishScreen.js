0537dfee1496777b0e3e9b2857378361fd4f313f
TuanDo <anhtuan7194@gmail.com>
2024-02-02 17:16:48 +0700

    Add Emoji for Result, Update Highscore Format

--- 10-react-quiz/src/components/FinishScreen.js
+++ 10-react-quiz/src/components/FinishScreen.js
@@ -1,11 +1,19 @@
 function FinishScreen({ points, maxPoints, highscore }) {
   const percentage = (points / maxPoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = '🥇';
+  if (80 <= percentage && percentage < 100) emoji = '🎉';
+  if (50 <= percentage && percentage < 80) emoji = '🙃';
+  if (0 < percentage && percentage < 50) emoji = '🤨';
+  if (percentage === 0) emoji = '🙇';
+
   return (
     <div>
       <p className="result">
-        You scored {points} out of {maxPoints} ({Math.ceil(percentage)} %)
+        <span>{emoji}</span> You scored {points} out of {maxPoints} (
+        {Math.ceil(percentage)} %)
       </p>
-      <p className="highscore">Highscore {highscore} (points)</p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
     </div>
   );
 }
