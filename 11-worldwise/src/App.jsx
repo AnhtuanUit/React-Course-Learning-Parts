1ad03340f45293e51dd12dcc7f5efb0d001c68a0
TuanDo <anhtuan7194@gmail.com>
2024-02-04 18:24:19 +0700

    Linking Between Routes With <Link /> and <NavLink />, Add Alias for Source Director

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import PageNotFound from './pages/PageNotFound';
-import Product from './pages/Product';
-import Pricing from './pages/Pricing';
 import HomePage from './pages/HomePage';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="home" element={<HomePage />} />
+        <Route path="" element={<HomePage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="*" element={<PageNotFound />} />
--- 11-worldwise/src/pages/PageNotFound.jsx
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -1,4 +1,11 @@
+import PageNav from '@src/components/PageNav';
+
 function PageNotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Page Not Found</h1>
+    </div>
+  );
 }
 export default PageNotFound;
