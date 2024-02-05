e32cadd0513b896e5e247c7bddc7a08d42bd954d
TuanDo <anhtuan7194@gmail.com>
2024-02-05 09:11:32 +0700

    Using CSS Modules

---
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from '@src/components/AppNav';
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>APP </p>
+    </div>
+  );
+}
+
+export default AppLayout;
--- 11-worldwise/src/pages/HomePage.jsx
+++ 11-worldwise/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
+import AppNav from '@src/components/AppNav';
 import PageNav from '@src/components/PageNav';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
     <div>
       <PageNav />
-      <h1>Worldwise</h1>
+      <AppNav />
+      <h1 className="test">Worldwise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
