378b2282446502a4feb97f62de3d13cf0ccfa4e5
TuanDo <anhtuan7194@gmail.com>
2024-02-17 09:57:59 +0700

    Adding Fake Authentication: Implementing "Login"

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -1,12 +1,14 @@
 import Map from '@src/components/Map';
 import Sidebar from '@src/components/Sidebar';
 import styles from './AppLayout.module.css';
+import User from '@src/components/User';
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
