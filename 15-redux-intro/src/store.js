409d49b18bfe7a52026ef8ee7dd960c8bad3663d
TuanDo <anhtuan7194@gmail.com>
2024-02-18 18:00:16 +0700

    Create a Redux Store

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -7,18 +9,20 @@ const initialState = {
 // eslint-disable-next-line no-unused-vars
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'acount/withdraw':
+    case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
-    case 'acount/deposit':
+    case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
-    case 'acount/requestLoan':
+    case 'account/requestLoan':
       if (state.loan > 0) return state;
       // LATER
       return {
         ...state,
-        balance: state.balance + action.payload,
+        balance: state.balance + action.payload.amount,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
       };
-    case 'acount/payLoan':
+    case 'account/payLoan':
       return {
         ...state,
         loan: 0,
@@ -29,3 +33,24 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+store.dispatch({ type: 'account/deposit', payload: 300 });
+console.log(store.getState());
+
+store.dispatch({ type: 'account/withdraw', payload: 50 });
+console.log(store.getState());
+
+store.dispatch({
+  type: 'account/requestLoan',
+  payload: {
+    amount: 500,
+    purpose: 'Buy a house',
+  },
+});
+console.log(store.getState());
+
+store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState());
