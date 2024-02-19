18b3c20312aa23cbc7ce519d86c824133a3e6ee8
AnhtuanUit <AnhtuanUit@users.noreply.github.com>
2024-02-19 07:42:48 +0000

    Add More State: Customer

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,13 +1,13 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const initialState = {
+const accountInitialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 };
 
 // eslint-disable-next-line no-unused-vars
-function reducer(state = initialState, action) {
+function accountReducer(state = accountInitialState, action) {
   switch (action.type) {
     case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
@@ -34,14 +34,41 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+const customerInitialState = {
+  fullName: '',
+  nationalID: '',
+};
 
-// console.log(store.getState());
+// eslint-disable-next-line no-unused-vars
+function customerReducer(state = customerInitialState, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: new Date().toISOString(),
+      };
+    case 'customer/deleteCustomer':
+      return customerInitialState;
+    default:
+      return state;
+  }
+}
+
+const rootReducers = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducers);
+
+// console.log(JSON.stringify(store.getState()));
 // store.dispatch({ type: 'account/deposit', payload: 300 });
-// console.log(store.getState());
+// console.log(JSON.stringify(store.getState()));
 
 // store.dispatch({ type: 'account/withdraw', payload: 50 });
-// console.log(store.getState());
+// console.log(JSON.stringify(store.getState()));
 
 // store.dispatch({
 //   type: 'account/requestLoan',
@@ -50,10 +77,10 @@ const store = createStore(reducer);
 //     purpose: 'Buy a house',
 //   },
 // });
-// console.log(store.getState());
+// console.log(JSON.stringify(store.getState()));
 
 // store.dispatch({ type: 'account/payLoan' });
-// console.log(store.getState());
+// console.log(JSON.stringify(store.getState()));
 
 // const ACCOUNT_DEPOSIT = 'account/deposit';
 
@@ -77,15 +104,30 @@ function payLoan() {
   return { type: 'account/payLoan' };
 }
 
-console.log(store.getState());
+console.log(JSON.stringify(store.getState()));
 store.dispatch(deposit(300));
-console.log(store.getState());
+console.log(JSON.stringify(store.getState()));
 
 store.dispatch(withdraw(50));
-console.log(store.getState());
+console.log(JSON.stringify(store.getState()));
 
 store.dispatch(requestLoan(500, 'Buy a house'));
-console.log(store.getState());
+console.log(JSON.stringify(store.getState()));
 
 store.dispatch(payLoan());
-console.log(store.getState());
+console.log(JSON.stringify(store.getState()));
+
+////////////////////////////////////////////////
+function createCustomer(fullName, nationalID) {
+  return { type: 'customer/createCustomer', payload: { fullName, nationalID } };
+}
+
+function deleteCustomer() {
+  return { type: 'customer/deleteCustomer' };
+}
+
+console.log(JSON.stringify(store.getState()));
+store.dispatch(createCustomer('Tuan', '123456'));
+console.log(JSON.stringify(store.getState()));
+store.dispatch(deleteCustomer());
+console.log(JSON.stringify(store.getState()));
