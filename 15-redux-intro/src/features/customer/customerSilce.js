578e524f371f2a06de95dfecedcfedf62d176797
AnhtuanUit <AnhtuanUit@users.noreply.github.com>
2024-02-19 10:53:54 +0000

    Creating the Customer Slice

--- 15-redux-intro/src/features/customer/customerSilce.js
+++ 15-redux-intro/src/features/customer/customerSilce.js
@@ -1,9 +1,34 @@
-const customerInitialState = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   fullName: '',
   nationalID: '',
 };
 
-export default function customerReducer(state = customerInitialState, action) {
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return { payload: { fullName, nationalID } };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = new Date().toISOString();
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
+
+/* export default function customerReducer(state = customerInitialState, action) {
   switch (action.type) {
     case 'customer/createCustomer':
       return {
@@ -25,4 +50,4 @@ export function createCustomer(fullName, nationalID) {
 
 export function deleteCustomer() {
   return { type: 'customer/deleteCustomer' };
-}
+} */
