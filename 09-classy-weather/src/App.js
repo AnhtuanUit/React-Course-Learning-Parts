2cbf6bc8d1d5cd989a387f21ecfd5e1ec9f40ce4
TuanDo <anhtuan7194@gmail.com>
2024-02-20 11:49:46 +0700

    Working With Event Handlers

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -4,14 +4,33 @@ export default class App extends React.Component {
     super(props);
 
     this.state = { count: 5 };
+    this.handleDec = this.handleDec.bind(this);
+    this.handleInc = this.handleInc.bind(this);
+  }
+
+  handleDec() {
+    this.setState(currState => {
+      return { count: currState.count - 1 };
+    });
+  }
+
+  handleInc() {
+    this.setState(currState => {
+      return { count: currState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date('june 21 2027');
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDec}>-</button>
+        <span>
+          {date.toDateString()} {this.state.count}
+        </span>
+        <button onClick={this.handleInc}>+</button>
       </div>
     );
   }
