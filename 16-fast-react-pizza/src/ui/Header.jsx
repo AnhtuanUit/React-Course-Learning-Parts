4c5e7e22582d6fe296ee766fbceb758587e0c458
TuanDo <anhtuan7194@gmail.com>
2024-02-25 15:41:48 +0700

    Building the App Layout

---
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Tuan</p>
+    </header>
+  );
+}
+
+export default Header;
