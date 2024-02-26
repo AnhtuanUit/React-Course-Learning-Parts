3f623a88f99582ad4c3be293df9365234626b68a
TuanDo <anhtuan7194@gmail.com>
2024-02-26 11:31:27 +0700

    Configuring Tailwind: Custom Font Family

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      pizza: 'Roboto Mono, monospace',
+    },
+    extend: {
+      colors: {
+        pizza: '#123456',
+      },
+      fontSize: {
+        huge: ['89rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
