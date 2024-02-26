234f0c837e8425717c65cd824201c01b7d6e2b4d
TuanDo <anhtuan7194@gmail.com>
2024-02-26 09:22:27 +0700

    Using Flex Box

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Tuan</div>;
+  return <div className="hidden text-sm font-semibold md:block">Tuan</div>;
 }
 
 export default Username;
