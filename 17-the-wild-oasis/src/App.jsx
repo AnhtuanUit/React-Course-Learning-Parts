4390c032d66320244ac5b2cd2e882cd98e82a48f
TuanDo <anhtuan7194@gmail.com>
2024-04-06 12:59:57 +0700

    Building the App Layout

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -9,6 +9,7 @@ import Account from './pages/Account';
 import NewUsers from './pages/Users';
 import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
@@ -16,15 +17,17 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="checkin/:bookingID" element={<CheckinBooking />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="users" element={<NewUsers />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="checkin/:bookingID" element={<CheckinBooking />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="account" element={<Account />} />
+          </Route>
           <Route path="login" element={<Login />} />
-          <Route path="account" element={<Account />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
