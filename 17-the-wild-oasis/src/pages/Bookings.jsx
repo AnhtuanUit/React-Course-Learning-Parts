5ee5d9fdd299350b013a994de21a47de951e8c7a
TuanDo <anhtuan7194@gmail.com>
2024-04-13 09:44:56 +0700

    API-Side Filtering: Filtering Bookings

--- 17-the-wild-oasis/src/pages/Bookings.jsx
+++ 17-the-wild-oasis/src/pages/Bookings.jsx
@@ -2,11 +2,13 @@ import BookingTable from "@src/features/bookings/BookingTable";
 import Heading from "../ui/Heading";
 
 import Row from "../ui/Row";
+import BookingTableOperations from "@src/features/bookings/BookingTableOperations";
 function Bookings() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
+        <BookingTableOperations />
       </Row>
       <Row>
         <BookingTable />
--- 17-the-wild-oasis/src/services/apiBookings.js
+++ 17-the-wild-oasis/src/services/apiBookings.js
@@ -1,10 +1,13 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
-    .from("bookings")
-    .select("*, cabins(*), guests(*)");
+export async function getBookings({ filter }) {
+  let query = supabase.from("bookings").select("*, cabins(*), guests(*)");
+
+  if (filter) {
+    query = query.eq(filter.field, filter.value);
+  }
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
