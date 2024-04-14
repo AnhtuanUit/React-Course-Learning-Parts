ad5471e5cbc9b9bb26e3f9feb4ca15c81fae615f
TuanDo <anhtuan7194@gmail.com>
2024-04-14 10:17:31 +0700

    Fix SortBy.jsx File Name

--- 17-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -1,6 +1,6 @@
-import Filter from '@src/ui/Filter';
-import TableOperations from '@src/ui/TableOperations';
-import SortBy from '@src/ui/sortBy';
+import Filter from "@src/ui/Filter";
+import TableOperations from "@src/ui/TableOperations";
+import SortBy from "@src/ui/SortBy";
 
 function CabinTableOperations() {
   return (
@@ -8,22 +8,22 @@ function CabinTableOperations() {
       <Filter
         filterField="discount"
         options={[
-          { label: 'All', value: 'all' },
-          { label: 'With Discount', value: 'with-discount' },
-          { label: 'No Discount', value: 'no-discount' },
+          { label: "All", value: "all" },
+          { label: "With Discount", value: "with-discount" },
+          { label: "No Discount", value: "no-discount" },
         ]}
       />
       <SortBy
         options={[
-          { label: 'Sort by name(A-Z)', value: 'name-asc' },
-          { label: 'Sort by name(Z-A)', value: 'name-desc' },
-          { label: 'Sort by price(low first)', value: 'regularPrice-asc' },
-          { label: 'Sort by price(high first)', value: 'regularPrice-desc' },
+          { label: "Sort by name(A-Z)", value: "name-asc" },
+          { label: "Sort by name(Z-A)", value: "name-desc" },
+          { label: "Sort by price(low first)", value: "regularPrice-asc" },
+          { label: "Sort by price(high first)", value: "regularPrice-desc" },
           {
-            label: 'Sort by capacity(high first)',
-            value: 'maxCapacity-desc',
+            label: "Sort by capacity(high first)",
+            value: "maxCapacity-desc",
           },
-          { label: 'Sort by capacity(low first)', value: 'maxCapacity-asc' },
+          { label: "Sort by capacity(low first)", value: "maxCapacity-asc" },
         ]}
       />
     </TableOperations>
