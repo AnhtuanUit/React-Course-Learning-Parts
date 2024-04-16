2113efdbac6e00c650f4bc02a534b354dfbdb7bd
TuanDo <anhtuan7194@gmail.com>
2024-04-16 14:31:45 +0700

    Building the Sign Up Form

--- 17-the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -1,27 +1,76 @@
-import Button from '../../ui/Button';
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import { useForm } from "react-hook-form";
+import Button from "../../ui/Button";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
+  const { getValues, register, formState, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  // console.log(formState.errors);
+
   return (
-    <Form>
-      <FormRow label="Full name" error={''}>
-        <Input type="text" id="fullName" />
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Full name" error={formState.errors.fullName?.message}>
+        <Input
+          type="text"
+          id="fullName"
+          {...register("fullName", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={''}>
-        <Input type="email" id="email" />
+      <FormRow label="Email address" error={formState.errors.email?.message}>
+        <Input
+          type="email"
+          id="email"
+          autoComplete="username"
+          {...register("email", {
+            required: "This field is required",
+            pattern: { value: /\S+@\S+\.\S+/, message: "Invalid email" },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={''}>
-        <Input type="password" id="password" />
+      <FormRow
+        label="Password (min 8 characters)"
+        error={formState.errors.password?.message}
+      >
+        <Input
+          type="password"
+          id="password"
+          autoComplete="new-password"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 8,
+              message: "Password length must be from 8",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={''}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow
+        label="Repeat password"
+        error={formState.errors.passwordConfirm?.message}
+      >
+        <Input
+          type="password"
+          id="passwordConfirm"
+          autoComplete="new-password"
+          {...register("passwordConfirm", {
+            required: "This field is required",
+            validate: (value) =>
+              value === getValues().password || "Confirm password not match",
+          })}
+        />
       </FormRow>
 
       <FormRow>
