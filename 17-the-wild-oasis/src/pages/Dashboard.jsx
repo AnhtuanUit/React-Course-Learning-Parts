7aaf5b700c7a3c43bcf8d02ed41fbde351363c03
TuanDo <anhtuan7194@gmail.com>
2024-04-17 13:13:36 +0700

    Building the Dashboard Layout

--- 17-the-wild-oasis/src/pages/Dashboard.jsx
+++ 17-the-wild-oasis/src/pages/Dashboard.jsx
@@ -1,12 +1,18 @@
+import DashboardLayout from "@src/features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import DashboardFilter from "@src/features/dashboard/DashboardFilter";
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+
+      <DashboardLayout />
+    </>
   );
 }
 
